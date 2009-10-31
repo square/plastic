4c1056c5c471cfe64753821ec09dbeeb68c17dd2
ydnar <ydnar@shaderlab.com>
2009-10-31 00:27:37 +0000

    Specs for #parse_track!

--- spec/plastic/track_spec.rb
+++ spec/plastic/track_spec.rb
@@ -7,10 +7,18 @@ describe Plastic do
 
   describe "#parse_tracks!" do
     it "calls #parse_track_2, then #parse_track_1" do
-      class FakeError < StandardError; end
       @instance.should_receive(:parse_track_2!).with().once
-      @instance.should_receive(:parse_track_1!).with().once.and_raise(FakeError)
-      lambda { @instance.parse_tracks! }.should raise_error(FakeError)
+      @instance.should_receive(:parse_track_1!).with().once.and_raise(StandardError)
+      lambda { @instance.parse_tracks! }.should raise_error(StandardError)
+    end
+  end
+
+  describe "#parse_track!" do
+    it "calls #parse_track_2, then #parse_track_1" do
+      arg = "foo"
+      @instance.should_receive(:parse_track_2!).with(arg).once
+      @instance.should_receive(:parse_track_1!).with(arg).once.and_raise(StandardError)
+      lambda { @instance.parse_track! arg }.should raise_error(StandardError)
     end
   end
 end
