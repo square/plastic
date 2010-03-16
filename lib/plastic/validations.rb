19aae434e0ed64bfe1f40ca5cb16d656585e906b
Cameron Walters <cameron@squareup.com>
2010-03-16 16:50:27 -0500

    Extracted expiration_datetime_year method.

--- lib/plastic/validations.rb
+++ lib/plastic/validations.rb
@@ -24,10 +24,10 @@ class Plastic
 
   def valid_expiration_year?
     this = Time.now.utc
-    (this.year..this.year + 20).include?(DateTime.strptime(expiration_year.to_s, "%y").year)
+    (this.year..this.year + 20).include?(expiration_datetime_year)
   end
 
-  def valid_expiration?
-    valid_expiration_month? && valid_expiration_year?
+  def expiration_datetime_year
+    DateTime.strptime(expiration_year.to_s, "%y").year
   end
 end
